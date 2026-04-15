e12dcfa9b6483d98dcfcaf3d4e4fc757d4c3592b
José Alisson <josealissondiasdacostaf10@gmail.com>
2026-04-15 13:57:40 -0300

    feat: added cors config

--- src/server/shared/Server.ts
+++ src/server/shared/Server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { router } from '../routes/router.js';
+import cors from "cors";
 import 'dotenv/config';
 import './services/TranslationsYup.js';
 
@@ -7,7 +8,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors({
+    origin: process.env.FRONTEND_HOST,
+}));
 
 app.use(router);
 
