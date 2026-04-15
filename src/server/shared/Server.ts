0c0b54fd9c8014b87af26053ee217d787e127a8a
José Alisson <josealissondiasdacostaf10@gmail.com>
2026-04-15 20:25:46 -0300

    fix: cors service

--- src/server/shared/Server.ts
+++ src/server/shared/Server.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_HOST,
+    origin: process.env.ENABLED_CORS?.split(";") || [],
 }));
 
 app.use(router);
